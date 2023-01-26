179d3701d8b397fca77cc29d7b6f230285a393e7
Russ Goetz <russgoetz@gmail.com>
2023-01-26 05:34:51 -0500

    WIP

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 fn main() {
     const LOG_CFG_FILE: &'static str = "log4rs.yml";
     println!("cargo:rerun-if-changed={}", LOG_CFG_FILE);
-    let tgt_dir = Path::new(env::var("CARGO_TARGET_DIR").unwrap().as_ref());
+    let tgt_dir = env::var("CARGO_TARGET_DIR").unwrap();
+    let tgt_dir = Path::new(&tgt_dir);
     fs::copy(LOG_CFG_FILE, tgt_dir.join(LOG_CFG_FILE)).expect("copoy log config file");
 }
